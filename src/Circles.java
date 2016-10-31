ad42ad9626046a0750a43205753eef0b369958cc
FuelDoks <fueldoks@gmail.com>
2016-10-31 14:51:37 +0300

    Re

---
+++ src/Circles.java
@@ -0,0 +1,23 @@
+import figures.Circle;
+
+public class Circles {
+    public static void main(String[] args) {
+
+        boolean a = true;
+        boolean b = false;
+
+
+
+                Circle circleA = new Circle(13, 11, 8);
+                Circle circleB = new Circle(12, 9, 6);
+                Circle circleC = new Circle(20, 17, 11);
+                Circle circleD = new Circle(24, 20, 19);
+
+                System.out.println("circleA " + circleA);
+                System.out.println("circleB " + circleB);
+                System.out.println("circleC " + circleC);
+                System.out.println("circleD " + circleD);
+
+    }
+
+}
